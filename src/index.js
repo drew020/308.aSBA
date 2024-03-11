ae9d8770f35573ce0da6362c7085269a190e1f37
Andrew McClain <86428905+drew020@users.noreply.github.com>
2024-03-11 00:44:57 -0400

    main functions work

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ let v_response;
 let v_data;
 
 const v_select = document.getElementById("card-select");
+const v_previous = document.getElementById("previous");
+const v_next = document.getElementById("next");
+
 const v_img = document.getElementById("image"); // Corrected to getElementsByClassName
 
-let v_selected_card;
+let v_selected_card = 0;
+
 
 // Fetch the data
 const f_fetch = async _ => {
@@ -31,11 +35,20 @@ const f_fetch = async _ => {
 const f_populate_selector = async () => {
     console.log(`f_populate_Selector has STARTED.`)
     for (let card of v_data.cards) { // corrected loop
-        const l_name = card.name;
-        const l_option_element = document.createElement(`option`);
-        l_option_element.setAttribute("value", l_name.toLowerCase());
-        l_option_element.textContent = l_name;
-        v_select.appendChild(l_option_element);
+        if (card.imageUrl != undefined) {
+            const l_name = card.name;
+            const l_option_element = document.createElement(`option`);
+            l_option_element.setAttribute("value", l_name.toLowerCase());
+            l_option_element.textContent = l_name;
+            v_select.appendChild(l_option_element);
+        }
+        else {
+            const l_name = 'NO_IMAGE';
+            const l_option_element = document.createElement(`option`);
+            l_option_element.setAttribute("value", l_name.toLowerCase());
+            l_option_element.textContent = l_name;
+            v_select.appendChild(l_option_element);
+        }
     }
     console.log(`f_populate_Selector has ENDED.`)
 }
@@ -44,19 +57,77 @@ const f_display_card = async (event) => {
     // Stops initial behavior
     event.preventDefault();
 
-    const l_index = event.target.selectedIndex +1;
+    const l_index = event.target.selectedIndex + 1;
     const l_pic_url = v_data.cards[l_index].imageUrl; // corrected accessing selected card
     try {
         v_img.setAttribute("src", l_pic_url); // corrected attribute setting
+        v_selected_card = l_index;
     } catch (error) {
         console.error(error);
     };
+
+    console.log(`card index: ${l_index - 1}`)
 }
 
-const f_clear_image = _ =>{
+function f_button(event) {
+    event.preventDefault();
+
+    if (event.target.textContent == 'NEXT') {
+        if (v_select.children.length > v_selected_card) {
+            v_selected_card++;
+        } try {
+            if (v_data.cards[v_selected_card].imageUrl) {
+                let l_pic_url = v_data.cards[v_selected_card].imageUrl; // corrected accessing selected card
+                v_img.setAttribute("src", l_pic_url); // corrected attribute setting
+                console.log(`Next Pressed:`)
+            } else {
+                if (v_select.children.length > v_selected_card) {
+                    v_selected_card++;
+                }
+                let l_pic_url = v_data.cards[v_selected_card].imageUrl; // corrected accessing selected card
+                v_img.setAttribute("src", l_pic_url); // corrected attribute setting
+                console.log(`Next Pressed:`)
+            }
+
+        } catch (error) {
+
+        }
+
+    }
+    else {
+        if (0 < v_selected_card) {
+            v_selected_card--;
+        } 
+        try {
+            if (v_data.cards[v_selected_card].imageUrl) {
+                let l_pic_url = v_data.cards[v_selected_card].imageUrl; // corrected accessing selected card
+                v_img.setAttribute("src", l_pic_url); // corrected attribute setting
+                console.log(`Previous Pressed:`)
+            }
+            else {
+                if (0 < v_selected_card) {
+                    v_selected_card--;
+                }
+                let l_pic_url = v_data.cards[v_selected_card].imageUrl; // corrected accessing selected card
+                v_img.setAttribute("src", l_pic_url); // corrected attribute setting
+                console.log(`Previous Pressed:`)
+            }
+        } catch (error) {
+
+        }
+
+    }
+
+    console.log(v_selected_card)
+
+}
+
+const f_clear_image = _ => {
     //
 }
 f_fetch();
 
 // Attach event listener
 v_select.addEventListener('change', f_display_card);
+v_next.addEventListener('click', f_button);
+v_previous.addEventListener('click', f_button);
